0806b9fc2169f67652bd1abd0ff7078cd13a99de
Esequiel Linares <esequiellinares06@gmail.com>
2026-02-20 17:42:38 -0500

    parsing through htm;

--- scrap/scrap.py
+++ scrap/scrap.py
@@ -1,3 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+
+def scrape_policy_page(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
