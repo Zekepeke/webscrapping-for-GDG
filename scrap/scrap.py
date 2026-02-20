89724204a1ff13a02477b22dd8dcd322dacf41e2
Esequiel Linares <esequiellinares06@gmail.com>
2026-02-20 17:47:55 -0500

    code

--- scrap/scrap.py
+++ scrap/scrap.py
@@ -16,3 +16,23 @@ def scrape_policy_page(url):
         "effective_date": "", # TODO need to find specific HTML tag for this
         "sections": []
     }
+    # Might need to change logic based on if the pages have different HTML structure
+    for header in soup.find_all('h2'):
+        section_title = header.get_text(strip=True)
+        section_text = []
+        
+        # Finding all sibling elements after header until the next header
+        for sibling in header.find_next_siblings():
+            if sibling.name == 'h2': # Stops when hit we hit next section
+                break
+            if sibling.name in ['p', 'ul', 'ol']: # Grab contents
+                section_text.append(sibling.get_text(separator=' ', strip=True))
+                
+        # Append the built section to our dictionary's list
+        if section_text:
+            policy_data["sections"].append({
+                "section_title": section_title,
+                "text": "\n".join(section_text)
+            })
+            
+    return policy_data
